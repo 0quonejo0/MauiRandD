e6af7b290240c621377ebd7fb02c39cb748f5717
0quonejo0 <mnpi.itm@outlook.com>
2022-09-05 11:30:39 +0800

    OfcPC update 23.Aug.2022

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,57 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using System.Collections.Specialized;
 
-public class BaseViewModel
+namespace MonkeyFinder.ViewModel;
+
+
+/* Changed event detector */
+public partial class BaseViewModel : ObservableObject
 {
+    public BaseViewModel()
+    {
+        
+    }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty]
+    string title;
+
+    public bool IsNotBusy => !IsBusy;
+
+
+    #region Working Codebase Reference
+    //public bool IsBusy
+    //{
+    //    get => isBusy;
+    //    set
+    //    {
+    //        if (isBusy == value)
+    //            return;
+    //        isBusy = value;
+    //        OnPropertyChanged();
+    //    }
+    //}
+    //public string Title
+    //{
+    //    get => title;
+    //    set
+    //    {
+    //        if (title == value)
+    //            return;
+    //        title = value;
+    //        OnPropertyChanged();
+    //        OnPropertyChanged(nameof(IsNotBusy));
+    //    }
+    //}
+
+    //public bool IsNotBusy => !isBusy;
+
+    //public event PropertyChangedEventHandler PropertyChanged;
+    //public void OnPropertyChanged([CallerMemberName]string name = null)
+    //{
+    //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    //}
+    #endregion
 }
--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,50 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
+
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    MonkeyService monkeyService;  // Get access to using MonkeyFinder.Services service
+    
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+
+    //public Command GetMonkeysCommand { get; } // MAUI internal implementation command
+
+    public MonkeysViewModel(MonkeyService monkeyService)  // Injecting the service to the View Model
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+    }
+
+    // Method to call the service above
+    
+    [RelayCommand]
+    async Task GetMonkeysAsync()
+    {
+        if (IsBusy)
+            return;
+        try
+        {
+            IsBusy = true;
+            var monkeys = await monkeyService.GetMonkeys();  // Get the data
+            if (Monkeys.Count != 0)
+                Monkeys.Clear();
+
+            foreach (var monkey in monkeys)
+                Monkeys.Add(monkey);  // Add to the collection, to the Observable
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!",
+                $"Unable to get data: {ex.Message}", "OK");  // 
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
 }
